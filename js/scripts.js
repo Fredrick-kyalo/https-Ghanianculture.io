7c375eb5e54d162308fe2025b5e9904d85789e44
fredrick-kyalo <kyalomwendwa44@gmail.com>
2020-02-17 09:35:20 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,103 @@
-unction getName() {
-    var year = document.getElementById("yob").value;
-    var month = document.getElementById("month").value;
-    var dayOfMonth = document.getElementById("day").value;
-    var year = document.getElementById("yob").value;
-    var date = new Date (`${dayOfMonth}/${month}/${year}`);
-    var date = new Date (`${year}-${month}-${dayOfMonth}`);
-    var birthDay = date.getDay();
-    var male = document.getElementById("male");
-    var female = document.getElementById("female");
- @-11,13 +11,13 @ function getName(){
-    var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
+function getAkanName(){
 
-    if (year == "" || year.length < "4" || year.length > "4"){
-    if (year == "" || year.length < 4 || year.length > 4){
-        alert("Hey you need to enter a valid year!");
+    var MM = document.querySelector("#month").value;
+    var YYYY = document.querySelector("#year").value;
+    var DD = document.querySelector("#day").value;
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+
+
+    if ((MM=="") || (isNaN(MM) || (MM < 0) || (MM>12))) {
+        alert('Invalid Month! Please enter correct Month');
+        return;
     }
-    else if (month < 0 || month > 12 || month == "" ){
-    else if (month < 0 || month > 12 ){
-        alert("Hey you need to enter a valid month!");
+
+    if ((YYYY=="") || (isNaN(YYYY) || (YYYY < 0))) {
+        alert('Invalid Year! Please enter correct Year');
+        return;
     }
-    else if( dayOfMonth < 0 || dayOfMonth >31 || dayOfMonth == "") {
-    else if( dayOfMonth < 0 || dayOfMonth >31) {
-        alert("Hey you need to re-enter a valid day!");
+
+
+    if ((DD=="") || (isNaN(DD) || (DD < 0) || (DD>31))) {
+        alert('Invalid Day of the Month! Please enter correct Day of the Month');
+        return;
     }
-    else if (male.checked == false && female.checked == false){
-@@ -30,7 +30,7 @@ function getName(){
-        result.innerHTML = "Hey " + femaleNames[birthDay] +  ". Well, since you were born on a " + days[birthDay] + " that's your Ghanaian name.";
+
+
+    var CC = YYYY.substring(0, 2)
+    var YY = YYYY.substring(2, 4)
+
+    // TODO Confirm why formular is inconsistent
+    // dayOfTheWeek = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+    var birthday =  new Date(YYYY, MM, DD);
+    var dayOfTheWeek = birthday.getDay();
+
+    var name = "";
+    // Sunday
+    if (dayOfTheWeek==1){
+        if(gender=="male"){
+            name = "Kwasi"
+        } else {
+            name = "Akosua"
+        }
     }
-    else{
-        alert("Come up Akan, just run!")
-        alert("Come on Akan, just run!")
+
+     // Monday
+     if (dayOfTheWeek==2){
+        if(gender=="male"){
+            name ="Kwadwo"
+        } else {
+            name ="Adwoa"
+        }
     }
+
+     // Tuesday
+     if (dayOfTheWeek==3){
+        if(gender=="male"){
+            name ="Kwabena"
+        } else {
+            name ="Abenaa"
+        }
+    }
+     // Wednesday
+     if (dayOfTheWeek==4){
+        if(gender=="male"){
+            name ="Kwaku"
+        } else {
+            name ="Akua"
+        }
+    }
+     // Thursday
+     if (dayOfTheWeek==5){
+        if(gender=="male"){
+            name ="Yaw"
+        } else {
+            name ="yaa"
+        }
+    }
+     // Friday
+     if (dayOfTheWeek==6){
+        if(gender=="male"){
+            name ="Kofi"
+        } else {
+            name ="Afua"
+        }
+    }
+     // Saturday
+     if (dayOfTheWeek==7){
+        if(gender=="male"){
+            name ="Kwame"
+        } else {
+            name ="Ama"
+        }
+    }
+
+    alert(
+        'The month is ' + MM + '\n'+
+        'The century is ' + CC+ '\n'+
+        'The year is ' + YY+ '\n'+
+        'The day is ' + DD + '\n'+
+        'The gender is ' + gender + '\n'+
+        'The day of the week born is ' + dayOfTheWeek  + '\n'+
+        'Your Akan name is ' + name )
 }
